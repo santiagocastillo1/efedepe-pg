42aa341c9b6df11556d76a2254ba9226ce6832c2
santiago <santiagocastillo766@gmail.com>
2024-10-10 23:30:07 -0300

    cambiando app

---
+++ src/layout/navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+
+
+export const Navbar = () => {
+    return(
+        <nav>
+            <h2>EFEDEPE.PG</h2>
+            <li>stickers</li>
+            <li>vinilos</li>
+            <li>materas</li>
+        </nav>
+    )
+} 
