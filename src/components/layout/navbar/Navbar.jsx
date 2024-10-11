6f14fa64e5b02e442def3161aaee33ae54d4369b
santiago <santiagocastillo766@gmail.com>
2024-10-11 00:35:54 -0300

    prolijidad

---
+++ src/components/layout/navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import "./navbar.css";
+
+export const Navbar = () => {
+  let nombre = "feli";
+  const sumar = () => {
+    console.log(2 + 2);
+  };
+
+  return (
+    <>
+      <div className="container-navbar ">
+        <h2>EFEDEPE.PG</h2>
+        <h2>{nombre}</h2>
+        <li>stickers</li>
+        <li>vinilos</li>
+        <li>materas</li>
+        <button onClick={sumar}>sumar</button>
+      </div>
+    </>
+  );
+};
